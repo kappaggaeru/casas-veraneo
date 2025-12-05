adc753a8c52780b98a70d95eeac42d0a5b9c97c9
Lautaro Olivera <oliveralautaro99@gmail.com>
2025-12-05 13:39:48 -0300

    ajuste en metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,33 +3,54 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Casa Cala",
-  description: "Departamentos de verano en Villa gesell",
+    title: "Casa Cala Alquiler Villa Gesell",
+    description: "Departamentos de verano en Villa gesell",
+    metadataBase: new URL("https://casa-cala.vercel.app/"),
+    openGraph: {
+        title: "Casa Cala Alquiler Villa Gesell",
+        description: "Departamentos de verano en Villa gesell",
+        url: "https://casa-cala.vercel.app/",
+        siteName: "Casa Cala",
+        type: "website",
+        images: [
+            {
+                url: "https://casa-cala.vercel.app/images/fachada.png",
+                width: 1200,
+                height: 630,
+                alt: "Casa Cala fachada",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Casa Cala Alquiler Villa Gesell",
+        description: "Departamentos de verano en Villa gesell",
+        images: ["https://casa-cala.vercel.app/images/fachada.png"],
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <title>Casa Cala Alquiler Villa Gesell</title>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
 }
